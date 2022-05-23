780a7031d2539bf193cb541c06e8571457a33bfb
xinzefeng <xinzefeng@ebupt.com>
2022-05-23 14:55:01 +0800

    [feat]:11;14;18

---
+++ type-challenges/easy/00011-easy-tuple-to-object/index.ts
@@ -0,0 +1,10 @@
+{
+  const tuple = ['tesla', 'model 3', 'model X', 'model Y'] as const
+
+type result = TupleToObject<typeof tuple> // expected { tesla: 'tesla', 'model 3': 'model 3', 'model X': 'model X', 'model Y': 'model Y'}
+
+//code 
+type TupleToObject<T extends readonly any[]> ={
+  [P in T[number]]:P
+}
+}
